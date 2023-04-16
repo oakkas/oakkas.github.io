5889f138dad5fdbaff047ec84f30df4b2f0fb5b2
oakkas-artheq <112590346+oakkas-artheq@users.noreply.github.com>
2023-04-15 23:23:43 -0600

    templatized navbar and footer

---
+++ components.js
@@ -0,0 +1,23 @@
+const navbar = `
+<nav class="navbar">
+  <div class="name-image-container">
+    <img class="profile-image" src="oguz.jpeg" alt="Profile Image">
+    <h1>Oguz Akkas</h1>
+  </div>
+  <ul>
+    <li><a href="index.html">Home</a></li>
+    <li><a href="research.html">Research</a></li>
+    <li><a href="resume.html">Resume</a></li>
+  </ul>
+</nav>
+`;
+
+const footer = `
+<footer>
+  <p>&copy; 2023 Oguz Akkas. All rights reserved.</p>
+  <div class="contact">
+    <p>Email: <a href="mailto:oakkas84@gmail.com">oakkas84@gmail.com</a></p>
+    <p>LinkedIn: <a href="https://www.linkedin.com/in/oakkas/" target="_blank">https://www.linkedin.com/in/oakkas/</a></p>
+  </div>
+</footer>
+`;
